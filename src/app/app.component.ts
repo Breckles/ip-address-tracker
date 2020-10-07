b1b5a6990c49586e9388fbcf666dbe31fa55693e
Waylon Carrier Nunes <wayloncn@gmail.com>
2020-10-07 18:41:43 -0400

    - installed leaflet - installed leaflet type definitions - successfully attempted the leaflet 'get started' tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ip-address-tracker-master';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    let map = L.map('map').setView([51.505, -0.09], 13);
+    console.log(map);
+
+    L.tileLayer(
+      'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken:
+          'pk.eyJ1Ijoid2F5bG9uY24iLCJhIjoiY2tmenluNG90MDhkcjMxbGVpdm53dW13ZSJ9.GZ6IWafdivwMazjFr5nzdg',
+      }
+    ).addTo(map);
+  }
 }
