a905eff24b2014b0272f5484e625063cef0ab7c2
Waylon Carrier Nunes <wayloncn@gmail.com>
2020-10-08 15:25:50 -0400

    played around with leaflet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit {
       'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
       {
         attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
         maxZoom: 18,
         id: 'mapbox/streets-v11',
         tileSize: 512,
@@ -24,5 +24,28 @@ export class AppComponent implements OnInit {
           'pk.eyJ1Ijoid2F5bG9uY24iLCJhIjoiY2tmenluNG90MDhkcjMxbGVpdm53dW13ZSJ9.GZ6IWafdivwMazjFr5nzdg',
       }
     ).addTo(map);
+
+    let locationIcon = L.icon({
+      iconUrl: '../assets/images/icon-location.svg',
+    });
+
+    let marker = L.marker([51.5, -0.09], { icon: locationIcon }).addTo(map);
+
+    let circle = L.circle([51.508, -0.11], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 500,
+    }).addTo(map);
+
+    var polygon = L.polygon([
+      [51.509, -0.08],
+      [51.503, -0.06],
+      [51.51, -0.047],
+    ]).addTo(map);
+
+    marker.bindPopup('<b>Hello world!</b><br>I am a popup.').openPopup();
+    circle.bindPopup('I am a circle.');
+    polygon.bindPopup('I am a polygon.');
   }
 }
