907e0a396505b1b650f842d9cd9dcbd43689608f
Waylon Carrier Nunes <wayloncn@gmail.com>
2020-10-08 15:37:35 -0400

    - created map component - moved map markup, styling, and functionality to map component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import * as L from 'leaflet';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  ngOnInit() {
-    let map = L.map('map').setView([51.505, -0.09], 13);
-    console.log(map);
-
-    L.tileLayer(
-      'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
-      {
-        attribution:
-          '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken:
-          'pk.eyJ1Ijoid2F5bG9uY24iLCJhIjoiY2tmenluNG90MDhkcjMxbGVpdm53dW13ZSJ9.GZ6IWafdivwMazjFr5nzdg',
-      }
-    ).addTo(map);
-
-    let locationIcon = L.icon({
-      iconUrl: '../assets/images/icon-location.svg',
-    });
-
-    let marker = L.marker([51.5, -0.09], { icon: locationIcon }).addTo(map);
-
-    let circle = L.circle([51.508, -0.11], {
-      color: 'red',
-      fillColor: '#f03',
-      fillOpacity: 0.5,
-      radius: 500,
-    }).addTo(map);
-
-    var polygon = L.polygon([
-      [51.509, -0.08],
-      [51.503, -0.06],
-      [51.51, -0.047],
-    ]).addTo(map);
-
-    marker.bindPopup('<b>Hello world!</b><br>I am a popup.').openPopup();
-    circle.bindPopup('I am a circle.');
-    polygon.bindPopup('I am a polygon.');
-  }
-}
+export class AppComponent {}
