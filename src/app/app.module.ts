84a533e679a8b9cb19edcf35445e4001d4d2f3df
Waylon Carrier Nunes <wayloncn@gmail.com>
2020-10-09 17:26:14 -0400

    - created data display component - marked up the component template - styled the component - styled the main app component to design spec

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { HeaderComponent } from './header/header.component';
+import { DataDisplayComponent } from './data-display/data-display.component';
 
 @NgModule({
-  declarations: [AppComponent, MapComponent, HeaderComponent],
+  declarations: [AppComponent, MapComponent, HeaderComponent, DataDisplayComponent],
   imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
